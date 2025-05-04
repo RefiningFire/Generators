713b321199c815cc943ce36bd548c5b8351b63b0
RefiningFire <93163433+RefiningFire@users.noreply.github.com>
2025-05-04 10:18:19 -0400

    v0.2 gems_table thru 100

--- Treasure Generator/treas_sub_types/gems_table.py
+++ Treasure Generator/treas_sub_types/gems_table.py
@@ -17,3 +17,15 @@ def gem_value(type):
         return [r.choice(['agate ','lapis lazuli ','tiger eye ','turquoise '])+type+'s worth 25gp each',1]
     elif roll <= 40:
         return [r.choice(['bloodstone ','crystal ','citrine ','jasper ','moonstone ','onyx '])+type+'s worth 50gp each',1]
+    elif roll <= 55:
+        return [r.choice(['carnelian ','chalcedony ','sardonyx ','zircon '])+type+'s worth 75gp each',1]
+    elif roll <= 70:
+        return [r.choice(['amber ','amethyst ','coral ','jade ','jet ','tourmaline '])+type+'s worth 100gp each',1]
+    elif roll <= 80:
+        return [r.choice(['garnet ','pearl ','spinel '])+type+'s worth 250gp each',1]
+    elif roll <= 90:
+        return [r.choice(['aquamarine ','alexandrite ','topaz '])+type+'s worth 500gp each',1]
+    elif roll <= 95:
+        return [r.choice(['opal ','star ruby, ','star sapphire ','sunset amethyst ','imperial topaz '])+type+'s worth 750gp each',1]
+    elif roll <= 100:
+        return [r.choice(['black sapphire, ','diamond ','emerald ','jacinth ','ruby '])+type+'s worth 1,000gp each',1]
